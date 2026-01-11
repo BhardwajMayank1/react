db28e7ba7666be5f47d02b4625033b19a71e6194
BhardwajMayank1 <mayank2006bhardwaj@gmail.com>
2026-01-12 00:50:11 +0530

    components

---
+++ 02-Components/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Cards from './components/Cards'
+import Navbar from './components/Navbar'
+
+
+
+const App = () => {
+  return (
+    <div>
+      
+       <Cards />
+       <Cards />
+       <Navbar />
+       <Navbar />
+       
+    </div>
+  )
+}
+
+export default App
+
+
+
---
+++ 02-Components/src/components/Cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='card'>
+        <h1>hey! i'm john doe</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, sint!</p>
+        
+    </div>
+  )
+}
+
+export default Cards
