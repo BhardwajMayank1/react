d6421d580ec6e303c61a6732646b5cb07fa3d291
BhardwajMayank1 <mayank2006bhardwaj@gmail.com>
2026-01-13 11:32:52 +0530

    UI project (props)

---
+++ src/components/section1/HeroText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div>
+      <div className='p-6'>
+            <h3 className='mb-7 leading-[1.1] text-5xl font-bold'>Prospective <br /> <span className='text-gray-600 ' >Customer</span> <br />Segmentation</h3>
+            <p className='text-xl w-[90%]font-medium text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quo ipsum omnis laborum cum dicta! Praesentium quod delectus aperiam inventore.</p>
+        </div>
+    </div>
+  )
+}
+
+export default HeroText
